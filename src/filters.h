ef0eb1d9fb7bcc23f405ef478c3316181088a249
Aurélien <aurelien.izoulet@epita.fr>
2022-05-31 02:31:55 +0200

    improved code

--- src/filters.h
+++ src/filters.h
@@ -9,9 +9,9 @@
 
 void grayscale(SDL_Surface* screen);
 void contrast_v1(SDL_Surface* img, int delta);
-void invert(SDL_Surface *image_surface, int width, int height);
-void edges_detection(SDL_Surface *image_surface, int threshold, int width, int height);
-void noiseReduction(SDL_Surface *surface, int width, int height);
+void invert(SDL_Surface *image_surface);
+void edges_detection(SDL_Surface *image_surface, int threshold);
+void noiseReduction(SDL_Surface *surface);
 void otsu(SDL_Surface *image_surface);
 
 
