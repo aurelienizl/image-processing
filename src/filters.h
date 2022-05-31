8e345be58b0c8160eb97c4440285c2c387ef44ca
Aurélien <aurelien.izoulet@epita.fr>
2022-05-31 02:24:08 +0200

    function name updated

--- src/filters.h
+++ src/filters.h
@@ -7,8 +7,8 @@
 
 //Filters functions:
 
-void grayscale(SDL_Surface* screen, int width, int height);
-void contrast_1(SDL_Surface* img, int delta);
+void grayscale(SDL_Surface* screen);
+void contrast_v1(SDL_Surface* img, int delta);
 void invert(SDL_Surface *image_surface, int width, int height);
 void edges_detection(SDL_Surface *image_surface, int threshold, int width, int height);
 void noiseReduction(SDL_Surface *surface, int width, int height);
